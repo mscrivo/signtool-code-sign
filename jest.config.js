1075a2efe96167201d1fbf9b2e5606d80b95122f
Michael Scrivo <mscrivo@gmail.com>
2024-12-21 13:25:41 -0500

    chore(lint): disable for jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
+// eslint-disable-next-line no-undef, importPlugin/no-commonjs
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
+	clearMocks: true,
+	moduleFileExtensions: ['js', 'ts'],
+	testEnvironment: 'node',
+	testMatch: ['**/*.test.ts'],
+	testRunner: 'jest-circus/runner',
+	transform: {
+		'^.+\\.ts$': 'ts-jest'
+	},
+	verbose: true
 }
