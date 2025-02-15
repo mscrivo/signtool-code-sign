9ed8e7b40cc1ba96cb1efdec0b3d6b7259499640
Michael Scrivo <mscrivo@gmail.com>
2025-02-15 09:57:54 -0500

    chore: various cleanups and lint fixes

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef, importPlugin/no-commonjs
+// eslint-disable-next-line import/no-commonjs, no-undef
 module.exports = {
 	clearMocks: true,
 	moduleFileExtensions: ['js', 'ts'],
